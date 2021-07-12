62e5da004a5d5766ee601eb25d3a2674eb678d0d
Rayza Barbosa <rayzasilvax@gmail.com>
2021-07-11 22:32:50 -0300

    Primeiro commit

---
+++ src/main/java/br/com/armariosifsp/armariosifsp/ArmariosifspApplication.java
@@ -0,0 +1,13 @@
+package br.com.armariosifsp.armariosifsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmariosifspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmariosifspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/armariosifsp/armariosifsp/ArmariosifspApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.armariosifsp.armariosifsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmariosifspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
